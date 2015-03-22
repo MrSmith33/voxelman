5cce55eaf0922bc03dab61cdd1b1bf293a0f3ed0
Andrey Penechko <mrsmith33@yandex.ru>
2015-03-22 13:56:19 +0200

    Add benchmark configuration

---
+++ source/benchmain.d
@@ -0,0 +1,16 @@
+/**
+Copyright: Copyright (c) 2015 Andrey Penechko.
+License: $(WEB boost.org/LICENSE_1_0.txt, Boost License 1.0).
+Authors: Andrey Penechko.
+*/
+module voxelman.benchmain;
+
+import std.experimental.logger;
+import voxelman.utils.log;
+
+void main(string[] args)
+{
+	setupLogger("bench.log");
+
+	info("benchmarking");
+}
