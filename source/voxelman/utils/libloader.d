d0940efbc05c2427c55df800b1c30562b46a46af
Andrey Penechko <mrsmith33@yandex.ru>
2015-12-20 15:26:55 +0200

    Implement lib loading for posix systems.

--- source/voxelman/utils/libloader.d
+++ source/voxelman/utils/libloader.d
@@ -8,12 +8,19 @@ module voxelman.utils.libloader;
 
 import voxelman.core.config;
 
+version(Posix)
+	enum DLL_SUFFIX = ".a";
+else version(Windows)
+	enum DLL_SUFFIX = ".dll";
+else static assert(false, "lib loading is not implemented for this platform");
+
+version(X86)
+	enum LIB_FOLDER = "/32/";
+else version(X86_64)
+	enum LIB_FOLDER = "/64/";
+else static assert(false, "lib loading is not implemented for this platform");
+
 string getLibName(string libName)
 {
-	version(Win32)
-		return LIB_PATH ~ "/32/" ~ libName ~ ".dll";
-	else version(Win64)
-		return LIB_PATH ~ "/64/" ~ libName ~ ".dll";
-	else
-		static assert(false, "lib loading is not implemented for this platform");
+	return LIB_PATH ~ LIB_FOLDER ~ libName ~ DLL_SUFFIX;
 }
